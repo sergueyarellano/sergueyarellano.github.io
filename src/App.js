11eb106ac4e7b1c1b9449b039a698bec2016e9ab
Serguey Martinez <sarellano85@gmail.com>
2020-04-28 19:51:34 -0400

    style: add css simple grid both desktop and mobile

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './style.css'
 
 const App = () => (
   <>
-    <header> my header</header>
+    <header>
+      <img style={{ gridArea: 'logo' }} src='myimage.jpg' loading='lazy' />
+      <h1 style={{ gridArea: 'title' }}>Polar EST +1</h1>
+      <nav style={{ gridArea: 'nav' }}>
+        <ul>
+          <li><a href='/home'>Home</a></li>
+          <li><a href='/about'>About</a></li>
+          <li><a href='/posts'>Posts</a></li>
+        </ul>
+      </nav>
+    </header>
     <main> my main content</main>
     <footer> my footer</footer>
   </>
