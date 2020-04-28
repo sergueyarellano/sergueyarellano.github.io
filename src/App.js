0a35fe10a60784f4629a35f6be276333e6f9f480
Serguey Martinez <sarellano85@gmail.com>
2020-04-28 17:49:39 -0400

    chore: set up react workflow

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+const App = () => (
+  <>
+    <header> my header</header>
+    <main> my main content</main>
+    <footer> my footer</footer>
+  </>
+)
+
+export default App
